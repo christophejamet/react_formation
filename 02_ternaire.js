25f0b82ff3ce27e12269d9d2a8e0e391cefdda0d
christophe jamet <christophe.jamet@darva.com>
2025-09-29 11:06:38 +0200

    02_ternaire.js

--- 02_ternaire.js
+++ 02_ternaire.js
@@ -6,7 +6,11 @@
  *    - utiliser l'opérateur ternaire (if interdit)
  */
 function ternaire(a , b) {
+    return a > b ? a : b;
 }
 
+console.log(ternaire(1,2));
+console.log(ternaire(2,1));
+console.log(ternaire(1,1));
 
 module.exports = ternaire;
